dc4dca8cb2091254922f05e1da76ae6c86eae646
knghtedbiiscuit <alibiscuit@gmail.com>
2017-02-10 02:32:53 +0800

    new

---
+++ WorkshopF/Qn2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace WorkshopF
+{
+    class Qn2
+    {
+        static void Main()
+        {
+            int[] marks = new int[] { 63, 29, 75, 82, 55 };
+            int arrlength = marks.Length; // what is length of array. Number of elements
+            int position = arrlength - 1; // nth position of array. starting with 0th.
+
+            for (int i = 0; i <= position - 1; i++)
+            {
+                for (int j = i + 1; j <= position; j++)
+                {
+                    if (marks[j] > marks[i])
+                    {
+                        int tempNum = marks[i];
+                        marks[i] = marks[j];
+                        marks[j] = tempNum;                      
+                    }
+
+                    for (int k = 0; k <= position; k++)
+                    {
+                        Console.Write("{0},", marks[k]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+
+            Console.Write("Final sorted: ");
+
+            for (int k = 0; k <= position; k++)
+            {
+                Console.Write("{0},", marks[k]);
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ WorkshopF/Qn3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace WorkshopF
+{
+    class Qn3
+    {
+        static void Main()
+        {
+            double[,] classmarks = new double[12, 6]
+                                { {56,84,68,29,0,0},
+                                  {94,73,31,96,0,0},
+                                  {41,63,36,90,0,0},
+                                  {99,9,18,17,0,0},
+                                  {62,3,65,75,0,0},
+                                  {40,96,53,23,0,0},
+                                  {81,15,27,30,0,0},
+                                  {21,70,100,22,0,0},
+                                  {88,50,13,12,0,0},
+                                  {48,54,52,78,0,0},
+                                  {64,71,67,25,0,0},
+                                  {16,93,46,72,0,0} };
+
+            
+            for (int i = 0; i < 12; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < 4; j++)
+                {
+                    double temp = classmarks[i,j];
+                    sum = sum + temp;
+                }
+                classmarks[i, 4] = sum;
+                double average = sum / 4;
+                classmarks[i,5] = average;
+            }
+
+            for (int k = 0; k < 12; k++)
+            {
+                Console.WriteLine("{0}, {1}",classmarks[k, 4],classmarks[k,5]);
+            }
+           
+
+
+        }
+
+    }
+}
